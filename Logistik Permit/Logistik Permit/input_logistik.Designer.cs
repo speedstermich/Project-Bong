12ae40209b07c27c17bce5c87423d9c2f074329e
Michael Octavianus Korintus <michael.201096@gmail.com>
2018-11-24 12:42:51 +0800

    Update

--- Logistik Permit/Logistik Permit/input_logistik.Designer.cs
+++ Logistik Permit/Logistik Permit/input_logistik.Designer.cs
@@ -42,27 +42,33 @@
 			this.textBox4 = new System.Windows.Forms.TextBox();
 			this.textBox5 = new System.Windows.Forms.TextBox();
 			this.textBox6 = new System.Windows.Forms.TextBox();
+			this.textBox7 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
 			this.SuspendLayout();
 			// 
 			// textBox1
 			// 
-			this.textBox1.Location = new System.Drawing.Point(303, 65);
+			this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox1.Location = new System.Drawing.Point(303, 52);
 			this.textBox1.Name = "textBox1";
-			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.Size = new System.Drawing.Size(289, 29);
 			this.textBox1.TabIndex = 0;
 			// 
 			// textBox2
 			// 
-			this.textBox2.Location = new System.Drawing.Point(303, 118);
+			this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox2.Location = new System.Drawing.Point(304, 105);
 			this.textBox2.Name = "textBox2";
-			this.textBox2.Size = new System.Drawing.Size(100, 20);
+			this.textBox2.Size = new System.Drawing.Size(288, 29);
 			this.textBox2.TabIndex = 1;
 			// 
 			// id_logistik
 			// 
 			this.id_logistik.AutoSize = true;
 			this.id_logistik.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.id_logistik.Location = new System.Drawing.Point(141, 60);
+			this.id_logistik.Location = new System.Drawing.Point(141, 47);
 			this.id_logistik.Name = "id_logistik";
 			this.id_logistik.Size = new System.Drawing.Size(92, 24);
 			this.id_logistik.TabIndex = 2;
@@ -72,7 +78,7 @@
 			// 
 			this.nama_barang.AutoSize = true;
 			this.nama_barang.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.nama_barang.Location = new System.Drawing.Point(141, 113);
+			this.nama_barang.Location = new System.Drawing.Point(141, 100);
 			this.nama_barang.Name = "nama_barang";
 			this.nama_barang.Size = new System.Drawing.Size(125, 24);
 			this.nama_barang.TabIndex = 3;
@@ -82,7 +88,7 @@
 			// 
 			this.jumlah.AutoSize = true;
 			this.jumlah.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.jumlah.Location = new System.Drawing.Point(141, 165);
+			this.jumlah.Location = new System.Drawing.Point(141, 152);
 			this.jumlah.Name = "jumlah";
 			this.jumlah.Size = new System.Drawing.Size(71, 24);
 			this.jumlah.TabIndex = 4;
@@ -92,7 +98,7 @@
 			// 
 			this.owner.AutoSize = true;
 			this.owner.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.owner.Location = new System.Drawing.Point(141, 212);
+			this.owner.Location = new System.Drawing.Point(141, 199);
 			this.owner.Name = "owner";
 			this.owner.Size = new System.Drawing.Size(113, 24);
 			this.owner.TabIndex = 5;
@@ -102,7 +108,7 @@
 			// 
 			this.keterangan.AutoSize = true;
 			this.keterangan.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.keterangan.Location = new System.Drawing.Point(141, 266);
+			this.keterangan.Location = new System.Drawing.Point(141, 253);
 			this.keterangan.Name = "keterangan";
 			this.keterangan.Size = new System.Drawing.Size(107, 24);
 			this.keterangan.TabIndex = 6;
@@ -112,7 +118,7 @@
 			// 
 			this.label1.AutoSize = true;
 			this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label1.Location = new System.Drawing.Point(140, 290);
+			this.label1.Location = new System.Drawing.Point(140, 277);
 			this.label1.Name = "label1";
 			this.label1.Size = new System.Drawing.Size(150, 16);
 			this.label1.TabIndex = 7;
@@ -122,7 +128,7 @@
 			// 
 			this.update_by.AutoSize = true;
 			this.update_by.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.update_by.Location = new System.Drawing.Point(141, 328);
+			this.update_by.Location = new System.Drawing.Point(141, 315);
 			this.update_by.Name = "update_by";
 			this.update_by.Size = new System.Drawing.Size(147, 24);
 			this.update_by.TabIndex = 8;
@@ -132,7 +138,7 @@
 			// 
 			this.label2.AutoSize = true;
 			this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.label2.Location = new System.Drawing.Point(142, 352);
+			this.label2.Location = new System.Drawing.Point(142, 339);
 			this.label2.Name = "label2";
 			this.label2.Size = new System.Drawing.Size(108, 16);
 			this.label2.TabIndex = 9;
@@ -140,37 +146,83 @@
 			// 
 			// textBox3
 			// 
-			this.textBox3.Location = new System.Drawing.Point(303, 170);
+			this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox3.Location = new System.Drawing.Point(303, 157);
 			this.textBox3.Name = "textBox3";
-			this.textBox3.Size = new System.Drawing.Size(100, 20);
+			this.textBox3.Size = new System.Drawing.Size(129, 29);
 			this.textBox3.TabIndex = 10;
 			// 
 			// textBox4
 			// 
-			this.textBox4.Location = new System.Drawing.Point(303, 217);
+			this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox4.Location = new System.Drawing.Point(303, 204);
 			this.textBox4.Name = "textBox4";
-			this.textBox4.Size = new System.Drawing.Size(100, 20);
+			this.textBox4.Size = new System.Drawing.Size(289, 26);
 			this.textBox4.TabIndex = 11;
 			// 
 			// textBox5
 			// 
-			this.textBox5.Location = new System.Drawing.Point(303, 271);
+			this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox5.Location = new System.Drawing.Point(303, 258);
 			this.textBox5.Name = "textBox5";
-			this.textBox5.Size = new System.Drawing.Size(100, 20);
+			this.textBox5.Size = new System.Drawing.Size(289, 29);
 			this.textBox5.TabIndex = 12;
 			// 
 			// textBox6
 			// 
-			this.textBox6.Location = new System.Drawing.Point(304, 333);
+			this.textBox6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox6.Location = new System.Drawing.Point(304, 320);
 			this.textBox6.Name = "textBox6";
-			this.textBox6.Size = new System.Drawing.Size(100, 20);
+			this.textBox6.Size = new System.Drawing.Size(288, 29);
 			this.textBox6.TabIndex = 13;
+			// 
+			// textBox7
+			// 
+			this.textBox7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.textBox7.Location = new System.Drawing.Point(518, 157);
+			this.textBox7.Name = "textBox7";
+			this.textBox7.Size = new System.Drawing.Size(74, 29);
+			this.textBox7.TabIndex = 14;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(441, 162);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(68, 24);
+			this.label3.TabIndex = 15;
+			this.label3.Text = "Satuan";
+			// 
+			// button1
+			// 
+			this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button1.Location = new System.Drawing.Point(303, 379);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(108, 46);
+			this.button1.TabIndex = 16;
+			this.button1.Text = "Add";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.button2.Location = new System.Drawing.Point(485, 379);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(107, 46);
+			this.button2.TabIndex = 17;
+			this.button2.Text = "Cancel";
+			this.button2.UseVisualStyleBackColor = true;
 			// 
 			// input_logistik
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.textBox7);
 			this.Controls.Add(this.textBox6);
 			this.Controls.Add(this.textBox5);
 			this.Controls.Add(this.textBox4);
@@ -208,5 +260,9 @@
 		private System.Windows.Forms.TextBox textBox4;
 		private System.Windows.Forms.TextBox textBox5;
 		private System.Windows.Forms.TextBox textBox6;
+		private System.Windows.Forms.TextBox textBox7;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
 	}
 }
